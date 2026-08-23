7fd1ae952d4e1ec66cf8602bdf788d115ffca76e
Pravat Behera <beherapravat301@gmail.com>
2026-08-23 18:08:20 +0530

    fix(prisma): update prisma.config.ts with DATABASE_URL fallback for build time

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, env } from 'prisma/config'
 export default defineConfig({
     schema: 'prisma/schema.prisma',
     datasource: {
-        url: env('DATABASE_URL'),
+        url: process.env.DATABASE_URL || "postgresql://postgres:postgres@localhost:5432/postgres",
     },
     migrations: {
         seed: 'npx tsx prisma/seed.ts',
